9fb73e4a5ad2f6ce52fc52db5b772a277ae1f2f4
Julian Rüth <julian.rueth@fsfe.org>
2021-12-01 16:37:56 -0600

    Change setrlimit with a patch
    instead of doing it in build.sh.
    
    Was there a good reason why this must happen in build.sh?
    
    It is also unclear why this change is necessary at all.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,3 @@
-if [[ "$(uname)" == "Darwin" ]]; then
-  sed -i.bak  "s/resource.setrlimit/#resource.setrlimit/g" rundoctests.py
-fi
 make build
 make install
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then

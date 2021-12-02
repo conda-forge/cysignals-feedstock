f6e489f72c14620c4486286b4b77b43d31cb6553
Julian Rüth <julian.rueth@fsfe.org>
2021-12-01 19:37:56 -0600

    Use cross-python when cross compiling

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -ex
 
-make configure
+make configure PYTHON="$PYTHON"
 ./configure --prefix=$PREFIX
-make build
-make install
+make build PYTHON="$PYTHON"
+make install PYTHON="$PYTHON"
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
   make test
 fi
